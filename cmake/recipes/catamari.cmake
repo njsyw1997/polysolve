95f8959d65196a0af5389c508d7a36ec701bc31e
njsyw1997 <shaoyw15@gmail.com>
2022-08-30 23:23:10 -0400

    Build catamari from Julian's fork

--- cmake/recipes/catamari.cmake
+++ cmake/recipes/catamari.cmake
@@ -1,24 +1,14 @@
+if(TARGET catamari::catamari)
+    return()
+endif()
 
 include(FetchContent)
 FetchContent_Declare(
     catamari
-    GIT_REPOSITORY https://gitlab.com/hodge_star/catamari.git
-    GIT_TAG 47e6369cb7139640ec74cec74020f2b3ebdf5983
+    GIT_REPOSITORY https://github.com/jpanetta/catamari.git
+    GIT_TAG 7bfe01b8864e965f9e7a87150e848ceaa56e9cb7
     GIT_SHALLOW FALSE
 )
+FetchContent_MakeAvailable(catamari)
 
-FetchContent_Declare(
-    mantis
-    GIT_REPOSITORY https://gitlab.com/hodge_star/mantis.git
-    GIT_TAG ceaed6ffb766b1e6a3cdcac726455d01525d8cd9
-    GIT_SHALLOW FALSE
-)
 
-FetchContent_Declare(
-    quotient
-    GIT_REPOSITORY https://gitlab.com/hodge_star/quotient.git
-    GIT_TAG cf8a9ae5853821e62320bb0be6c87fa6ad1dec36
-    GIT_SHALLOW FALSE
-)
-
-FetchContent_MakeAvailable(catamari mantis quotient)
