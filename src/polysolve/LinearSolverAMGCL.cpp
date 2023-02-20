1bae3c781f29a37f547dd944250c4d4fb52a15dd
njsyw1997 <shaoyw15@gmail.com>
2023-02-20 11:32:57 -0500

    delete extra code

--- src/polysolve/LinearSolverAMGCL.cpp
+++ src/polysolve/LinearSolverAMGCL.cpp
@@ -287,7 +287,7 @@ namespace polysolve
 
         std::vector<double> null;
         int nv;
-        nv = amgcl::coarsening::rigid_body_modes(BLOCK_SIZE, coo, null); // COO requires to change the form of vector<double>
+        nv = amgcl::coarsening::rigid_body_modes(BLOCK_SIZE, coo, null); // TODO: COO requires to change the form of vector<double>
         pt_params.put("precond.coarsening.nullspace.cols", nv);
         pt_params.put("precond.coarsening.nullspace.B",    &null[0]);
 
@@ -338,145 +338,9 @@ namespace polysolve
     {
     }
 
-
-    template <int BLOCK_SIZE>
-    LinearSolverAMGCL_NS<BLOCK_SIZE>::LinearSolverAMGCL_NS()
-    {
-        // NOTE: usolver and psolver parameters are only used if the
-        // preconditioner class is "schur_pressure_correction"
-        precond_num_ = 0;
-    }
-
-    template <int BLOCK_SIZE>
-    void LinearSolverAMGCL_NS<BLOCK_SIZE>::setParameters(const json &params)
-    {
-        set_params(params, params_);
-    }
-
-    template <int BLOCK_SIZE>
-    void LinearSolverAMGCL_NS<BLOCK_SIZE>::getInfo(json &params) const
-    {
-        params["num_iterations"] = iterations_;
-        params["final_res_norm"] = residual_error_;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    template <int BLOCK_SIZE>
-    void LinearSolverAMGCL_NS<BLOCK_SIZE>::factorize(const StiffnessMatrix &Ain)
-    {
-        assert(precond_num_ > 0);
-
-        int numRows = Ain.rows();
-
-        WrappedArray<StiffnessMatrix::StorageIndex> ia(Ain.outerIndexPtr(), numRows + 1);
-        WrappedArray<StiffnessMatrix::StorageIndex> ja(Ain.innerIndexPtr(), Ain.nonZeros());
-        WrappedArray<StiffnessMatrix::Scalar> a(Ain.valuePtr(), Ain.nonZeros());
-
-        if (params_["precond"]["class"] == "schur_pressure_correction")
-        {
-            std::vector<char> pmask(numRows, 0);
-            for (size_t i = precond_num_; i < numRows; ++i)
-                pmask[i] = 1;
-            params_["precond"]["pmask"] = pmask;
-        }
-
-        // AMGCL takes the parameters as a Boost property_tree (i.e., another JSON data structure)
-        std::stringstream ss_params;
-        ss_params << params_;
-        boost::property_tree::ptree pt_params;
-        boost::property_tree::read_json(ss_params, pt_params);
-
-        auto A = std::tie(numRows, ia, ja, a);
-        auto Ab = amgcl::adapter::block_matrix<dmat_type>(A);
-        prof.tic("setup");
-        solver_ = std::make_unique<Solver>(Ab, pt_params);
-        prof.toc("setup");
-        iterations_ = 0;
-        residual_error_ = 0;
-    }
-
-    template <int BLOCK_SIZE>
-    void LinearSolverAMGCL_NS<BLOCK_SIZE>::factorize(const StiffnessMatrix &Ain, const VectorXd &coo)
-    {
-        assert(precond_num_ > 0);
-
-        int numRows = Ain.rows();
-
-        WrappedArray<StiffnessMatrix::StorageIndex> ia(Ain.outerIndexPtr(), numRows + 1);
-        WrappedArray<StiffnessMatrix::StorageIndex> ja(Ain.innerIndexPtr(), Ain.nonZeros());
-        WrappedArray<StiffnessMatrix::Scalar> a(Ain.valuePtr(), Ain.nonZeros());
-
-        if (params_["precond"]["class"] == "schur_pressure_correction")
-        {
-            std::vector<char> pmask(numRows, 0);
-            for (size_t i = precond_num_; i < numRows; ++i)
-                pmask[i] = 1;
-            params_["precond"]["pmask"] = pmask;
-        }
-
-        // AMGCL takes the parameters as a Boost property_tree (i.e., another JSON data structure)
-        std::stringstream ss_params;
-        ss_params << params_;
-        boost::property_tree::ptree pt_params;
-        boost::property_tree::read_json(ss_params, pt_params);
-        typename Solver::params prm(pt_params);
-
-        prm.precond.coarsening.nullspace.cols = amgcl::coarsening::rigid_body_modes(
-        BLOCK_SIZE, coo, prm.precond.coarsening.nullspace.B);
-
-        auto A = std::tie(numRows, ia, ja, a);
-        auto Ab = amgcl::adapter::block_matrix<dmat_type>(A);
-        prof.tic("setup");
-        solver_ = std::make_unique<Solver>(Ab, prm);
-        prof.toc("setup");
-        iterations_ = 0;
-        residual_error_ = 0;
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    template <int BLOCK_SIZE>
-    void LinearSolverAMGCL_NS<BLOCK_SIZE>::solve(const Eigen::Ref<const VectorXd> rhs, Eigen::Ref<VectorXd> result)
-    {
-        assert(result.size() == rhs.size());
-        std::vector<double> _rhs(rhs.data(), rhs.data() + rhs.size());
-        // if (is_test)
-        // {
-        //     result=Eigen::VectorXd::Random(result.size());
-        // }
-        std::vector<double> x(result.data(), result.data() + result.size());
-        auto rhs_b = amgcl::backend::reinterpret_as_rhs<dmat_type>(_rhs);
-        auto x_b = amgcl::backend::reinterpret_as_rhs<dmat_type>(x);
-
-        assert(solver_ != nullptr);
-        prof.tic("solve");
-        std::tie(iterations_, residual_error_) = (*solver_)(rhs_b, x_b);
-        prof.toc("solve");
-        for (size_t i = 0; i < rhs.size() / BLOCK_SIZE; i++)
-            for (size_t j = 0; j < BLOCK_SIZE; j++)
-            {
-                result[BLOCK_SIZE * i + j] = x_b[i](j);
-            }
-        if (is_test)
-        {
-            std::cout << (*solver_) << std::endl;
-            std::cout << prof << std::endl;
-        }
-    }
-
-    template <int BLOCK_SIZE>
-    LinearSolverAMGCL_NS<BLOCK_SIZE>::~LinearSolverAMGCL_NS()
-    {
-    }
-
-
     template class LinearSolverAMGCL_Block<2>;
     template class LinearSolverAMGCL_Block<3>;
-    template class LinearSolverAMGCL_NS<2>;
-    template class LinearSolverAMGCL_NS<3>;
+
 } // namespace polysolve
 
 #endif
